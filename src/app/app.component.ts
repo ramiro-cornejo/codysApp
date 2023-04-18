94dd620c5acd4f6750df83940cf2565b4d54a466
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2023-04-17 21:49:48 -0300

    Estructura de proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'codysApp';
+  imgCody = 'assets/images/1.png';
+
+  ngOnInit(){
+    this.getCodyImg();
+  }
+
+  getCodyImg(){
+    let rand = Math.random() * 10 + 1;
+    rand = Math.floor(rand);
+    this.imgCody = `assets/images/${rand}.png`;
+  }
 }
